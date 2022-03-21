df271f6a35db05924f8b4bc58a9354e566a3ec6a
Alex Kavvos <alex.kavvos@bristol.ac.uk>
2022-03-21 00:49:31 +0000

    updating JS code

--- code/javascript/bristol-app/script.js
+++ code/javascript/bristol-app/script.js
@@ -6,11 +6,12 @@ window.onload = function () {
 }
 
 function populateWards(wards) {
- let buttons = new DocumentFragment();
+  let buttons = new DocumentFragment();
 
   wards.records.forEach(w => {
       const [id, name] = [w.record.fields.ward_id, w.record.fields.name];
-      const b = createElementText("button", name);
+      const b = document.createElement("button");
+      b.textContent = name;
       b.onclick = displayData(id, name);
       buttons.appendChild(b);
   });
@@ -20,97 +21,55 @@ function populateWards(wards) {
   nav.append(buttons);
 }
 
-function createElementWith(tag, xs) {
-  let elem = document.createElement(tag);
-  for (const x of xs) {
-    elem.appendChild(x);
-  }
-  return elem;
-}
-
-function createElementText(tag, text) {
-  let elem = document.createElement(tag);
-  elem.textContent = text;
-  return elem;
-}
-
-function createElementHTML(tag, html) {
-  let elem = document.createElement(tag);
-  elem.innerHTML = html;
-  return elem;
-}
-
 function displayData(id, name) {
   
-  function buildWardPopulation(records) {
+  function buildPopulation(records) {
 
     // Make heading
-    let heading = createElementText('h2', 'Population');
+    let heading = document.createElement('h2');
+    heading.textContent = 'Population';
 
     // Make table
-    let table = createElementWith('table',
-      [createElementHTML('tr', '<th>Year</th><th>Population</th>')].concat(
-          records.filter(d => d.record.fields.mid_year >= 2015)
-            .sort((x1, x2) => x1.record.fields.mid_year < x2.record.fields.mid_year ? -1 : 1)
-            .map(r =>
-              createElementWith('tr', [
-                createElementText('td', r.record.fields.mid_year),
-                createElementText('td', r.record.fields.population_estimate)
-              ])
-            )
-        )
-    );
+    let table = document.createElement('table');
     table.setAttribute('id','populationTable');
+
+    // Make table header
+    let header = document.createElement('tr');
+    header.innerHTML = '<th>Year</th><th>Population</th></tr>';
+    table.appendChild(header);
+    
+    // Populate table
+    records.sort((x1, x2) => x1.record.fields.mid_year < x2.record.fields.mid_year ? -1 : 1)
+      .forEach(r => {
+        let year = document.createElement('td');
+        year.textContent = r.record.fields.mid_year;
+        let population = document.createElement('td');
+        population.textContent = r.record.fields.population_estimate;
+
+        let row = document.createElement('tr');
+        row.append(year, population);
+        table.appendChild(row);
+    });
     
     let population = new DocumentFragment();
     population.append(heading, table);
     
     return population;
   }
-  
-  function buildWardLifeExpectancy(records) {
-
-    // Make heading
-    let heading = createElementText('h2', 'Life expectancy (2018-2020)');
-
-    // There should be a unique record
-    let [r] = records.filter(d => d.record.fields.year === '2018-2020')
-    
-    // Make table
-    let table = createElementWith('table', [
-        createElementWith('tr', [
-          createElementText('td', 'Male life expectancy'),
-          createElementText('td', Math.round(r.record.fields.male_life_expectancy))
-        ]),
-        createElementWith('tr', [
-          createElementText('td', 'Female life expectancy'),
-          createElementText('td', Math.round(r.record.fields.female_life_expectancy))
-        ])
-    ]);
-    table.setAttribute('id', 'lifeExpectancyTable');
-
-    let lifeExpectancy = new DocumentFragment();
-    lifeExpectancy.append(heading, table);
-    
-    return lifeExpectancy;
-  }
 
   return function () {
-    Promise.all(
-      [
-        fetch(`https://opendata.bristol.gov.uk/api/v2/catalog/datasets/population-estimates-time-series-ward/records?limit=20&select=mid_year,population_estimate&refine=ward_2016_code:${id}`),
-        fetch(`https://opendata.bristol.gov.uk/api/v2/catalog/datasets/life-expectancy-in-bristol/records?limit=20&refine=ward_code:${id}`)
-      ]
-    )
-    .then(resps => Promise.all(resps.map(d => d.json())))
-    .then(([population, lifeExpectancy]) => {
-          let dataPane = document.getElementById("dataPane");
-          dataPane.textContent = '';
-          dataPane.append(
-            createElementText('h1', name),
-            buildWardPopulation(population.records),
-            buildWardLifeExpectancy(lifeExpectancy.records)
-          );
-    });
+    let wards = fetch(`https://opendata.bristol.gov.uk/api/v2/catalog/datasets/population-estimates-time-series-ward/records?limit=20&select=mid_year,population_estimate&refine=ward_2016_code:${id}`)
+      .then(response => response.json())
+      .then(data => {
+        let heading = document.createElement('h1');
+        heading.textContent = name;
+        
+        let population = buildPopulation(data.records);
+
+        let dataPane = document.getElementById("dataPane");
+        dataPane.textContent = '';
+        dataPane.append(heading, population);
+      })
+      .catch(err => console.log(err));
   }
 }
